2598bf65c93f9433dee7e1400e9945a8487a90a2
Julian Yates <julianyates@Julians-MacBook-Pro.local>
2019-06-02 14:22:37 -0500

    mvp done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = props => {
+    return (
+        <>
+        <div className="content-container">
+            <h1>Get Started with React</h1>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p className='lite-grey'>reactjs.org</p>
+        </div>
+        </>
+    )
+};
+
+
+export default CardContent;
